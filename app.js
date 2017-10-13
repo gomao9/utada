88024a04cb168bd01921ad262dac6d3fe2b41ae9
gomao9 <gomaoq@gmail.com>
2017-10-13 11:15:33 +0900

    fix to use class

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
 Vue.use(Buefy.default)
+
+class Song {
+  constructor(hash) {
+    this.name         = hash[':name']
+    this.url          = hash[':url']
+    this.album        = hash[':album_short']
+    this.singers_text = hash[':singers'].join('、');
+
+    var share_text = `
+【${this.name}】(${this.singers_text})
+${this.url}
+    `.trim();
+    this.imastodon_share = encodeURI('https://imastodon.net/share?text=' + share_text);
+    this.twitter_share   = encodeURI('https://twitter.com/share?text='   + share_text);
+  }
+}
+
 var app = new Vue({
   el: '#app',
   data: {
@@ -16,19 +33,7 @@ var app = new Vue({
       var songs = YAML.load('https://bitbucket.org/gomao9/utada_data/raw/83cc065fc2ae402c0375181a47df4e97c95039b6/million.yml').concat(
         YAML.load('https://bitbucket.org/gomao9/utada_data/raw/83cc065fc2ae402c0375181a47df4e97c95039b6/cinderella.yml'));
       return songs.map(function(song) {
-        song.name         = song[':name']
-        song.url          = song[':url']
-        song.album        = song[':album_short']
-        song.singers_text = song[':singers'].join('、');
-
-        var share_text = `
-【${song.name}】(${song.singers_text})
-${song.url}
-        `.trim();
-        song.imastodon_share = encodeURI('https://imastodon.net/share?text=' + share_text);
-        song.twitter_share   = encodeURI('https://twitter.com/share?text='   + share_text);
-
-        return song;
+        return new Song(song);
       });
     }
   },
