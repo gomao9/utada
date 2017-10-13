62e91321f7b725b7e58cd94fdb07f68882195926
gomao9 <gomaoq@gmail.com>
2017-10-13 11:36:20 +0900

    add cd column and search

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 Vue.use(Buefy.default)
 
 class Song {
-  constructor(hash) {
+  constructor(hash, cds) {
     this.name         = hash[':name']
     this.url          = hash[':url']
-    this.album        = hash[':album_short']
     this.singers_text = hash[':singers'].join('、');
 
     var share_text = `
@@ -13,6 +12,18 @@ ${this.url}
     `.trim();
     this.imastodon_share = encodeURI('https://imastodon.net/share?text=' + share_text);
     this.twitter_share   = encodeURI('https://twitter.com/share?text='   + share_text);
+
+    this.cd = cds.find(function (cd) {
+      return cd.name_short == hash[':album_short'];
+    });
+  }
+}
+
+class CD {
+  constructor(hash) {
+    this.name         = hash[':name'];
+    this.name_short   = hash[':name_short'];
+    this.series_short = hash[':series_short'];
   }
 }
 
@@ -24,24 +35,34 @@ var app = new Vue({
     songs: undefined
   },
   mounted: function () {
-    var songs = this.get_songs();
+    var cds = this.get_cds();
+    var songs = this.get_songs(cds);
     this.original_songs = songs;
     this.songs = songs;
   },
   methods: {
-    get_songs: function() {
+    get_songs: function (cds) {
       var songs = YAML.load('https://bitbucket.org/gomao9/utada_data/raw/83cc065fc2ae402c0375181a47df4e97c95039b6/million.yml').concat(
         YAML.load('https://bitbucket.org/gomao9/utada_data/raw/83cc065fc2ae402c0375181a47df4e97c95039b6/cinderella.yml'));
       return songs.map(function(song) {
-        return new Song(song);
+        return new Song(song, cds);
+      });
+    },
+    get_cds: function () {
+      var cds = YAML.load('https://bitbucket.org/gomao9/utada_data/raw/83cc065fc2ae402c0375181a47df4e97c95039b6/albums.yml')
+      return cds.map(function (cd) {
+        return new CD(cd);
       });
     }
   },
   watch: {
     keyword: function (word) {
+      word = word.toLowerCase();
       this.songs = this.original_songs.filter(function(song) {
-        return song.name.toLowerCase().includes(word.toLowerCase() || '') ||
-               song.singers_text.includes(word || '');
+        var song_name = song.name.toLowerCase();
+        var singer = song.singers_text;
+        var cd_name = song.cd.name.toLowerCase();
+        return song_name.includes(word || '') || singer.includes(word || '') || cd_name.includes(word || '');
       });
     }
   }
